ff5a218305b4fe0a0d359610fa63e051808f5b29
jflahaut <30655859+jeremieflahaut@users.noreply.github.com>
2023-08-14 16:05:02 +0200

    Update index.blade.php

--- resources/views/livewire/posts/index.blade.php
+++ resources/views/livewire/posts/index.blade.php
@@ -4,7 +4,7 @@
             <div class="col-md-4 mb-4">
                 <div class="card h-100">
                     <div class="card-body d-flex flex-column">
-                        <img src="{{ asset($post->image_path) }}" class="card-img-top" alt="Article Image">
+                        <img src="{{ asset($post->image_path) }}" class="card-img-top" alt="{{ $post->title }}">
                         <h5 class="card-title">{{ $post->title }}</h5>
                         <p class="text-muted">Mise à jour le : {{ $post->updated_at->format('d/m/Y') }}</p>
                         <p class="card-text">{{ $post->limitedContent() }}</p>

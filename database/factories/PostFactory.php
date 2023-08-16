26fc2694afbd6501d7897a34e0737610455fc0bc
Jeremie <jeremieflahaut@gmail.com>
2023-08-16 16:02:13 +0200

    fix folder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,12 +4,15 @@ namespace Database\Factories;
 
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\File;
 
 /**
  * @extends Factory<Post>
  */
 class PostFactory extends Factory
 {
+    const FOLDER = 'images/articles';
+
     /**
      * Define the model's default state.
      *
@@ -17,11 +20,19 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $path = public_path(self::FOLDER);
+
+        if (!File::exists($path)) {
+            File::makeDirectory($path);
+        }
+
+        File::cleanDirectory($path);
+
         return [
             'title' => fake()->sentence,
             'description' => fake()->text(150),
             'content' => fake()->paragraphs(3, true),
-            'image_path' => 'images/' . fake()->image(public_path('images'), 400, 300, null, false),
+            'image_path' => self::FOLDER . '/' . fake()->image($path, 400, 300, null, false),
             'featured' => fake()->boolean()
         ];
     }

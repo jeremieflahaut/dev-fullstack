1b3e099fe8be6cd3f990cc7ecfcdd3cf9762259f
Jeremie <jeremieflahaut@gmail.com>
2023-09-08 16:47:04 +0200

    deploy sh

---
+++ server_deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintanance mode
+sudo docker exec -ti dev_fullstack_web php artisan down
+    # Update codebase
+    git pull origin master
+
+    # Install dependencies based on lock file
+    sudo docker exec -ti dev_fullstack_web  composer install --no-interaction --prefer-dist --optimize-autoloader
+
+    # Migrate database
+    sudo docker exec -ti dev_fullstack_web  php artisan migrate --force
+
+    # Clear cache
+    sudo docker exec -ti dev_fullstack_web  php artisan optimize
+
+# Exit maintenance mode
+sudo docker exec -ti dev_fullstack_web php artisan up
+
+echo "🚀 Application deployed!"

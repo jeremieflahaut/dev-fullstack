1a63f5637a0248c7eb9924320cb1de0d05f7c370
jflahaut <30655859+jeremieflahaut@users.noreply.github.com>
2023-09-11 09:41:21 +0200

    Update server_deploy.sh

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,21 +4,21 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintanance mode
-sudo docker exec dev_fullstack_web php artisan down
+docker exec dev_fullstack_web php artisan down
     # Update codebase
     git pull origin master
 
     # Install dependencies based on lock file
-    sudo docker exec dev_fullstack_web composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+    docker exec dev_fullstack_web composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
     # Migrate database
-    sudo docker exec dev_fullstack_web php artisan migrate --force
+    docker exec dev_fullstack_web php artisan migrate --force
 
     # Clear cache
-    sudo docker exec dev_fullstack_web php artisan config:clear
-    sudo docker exec dev_fullstack_web php artisan route:clear
+    docker exec dev_fullstack_web php artisan config:clear
+    docker exec dev_fullstack_web php artisan route:clear
 
 # Exit maintenance mode
-sudo docker exec dev_fullstack_web php artisan up
+docker exec dev_fullstack_web php artisan up
 
 echo "🚀 Application deployed!"

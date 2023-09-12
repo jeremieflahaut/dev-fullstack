8ee1b114b5601cff5d0c10b9bb6735627197ea49
jflahaut <30655859+jeremieflahaut@users.noreply.github.com>
2023-09-12 15:12:27 +0200

    Auth Tests (#3)
    * start pest
    
    * start welcome tests
    
    * update README and fix github workflow filenames
    
    * fix phpunit
    
    * fix phpunit
    
    * adding unit test example
    
    * fix workflow name
    
    * adding tests badge
    
    * fix
    
    * typo
    
    * typo
    
    * test badge
    
    * test badge
    
    * test badge
    
    * badges
    
    * adding Auth Tests
    
    * fix badge

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -1,19 +1,23 @@
 <?php
 
+use App\Http\Livewire\Layout\Footer;
+use App\Http\Livewire\Layout\Header;
 use function Pest\Livewire\livewire;
 
 it('welcome page', function () {
-    $response = $this->get('/');
-    $response->assertOk();
+    $this->get('/')
+        ->assertOk()
+        ->assertSeeLivewire(Header::class)
+        ->assertSeeLivewire(Footer::class);
 });
 
 it('see header component', function() {
-    livewire(\App\Http\Livewire\Layout\Header::class)
+    livewire(Header::class)
         ->assertSee('Accueil');
 });
 
 it('see footer component', function() {
-    livewire(\App\Http\Livewire\Layout\Footer::class)
+    livewire(Footer::class)
         ->assertSee('Accueil')
         ->assertSee('Mentions Légales')
         ->assertSee('Politique de Cookies');
